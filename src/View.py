790e1f584d7ae7acf825e401a5a35936e5d58c0a
hkolanska <helenakolanska@gmail.com>
2019-05-22 08:52:58 +0200

    Adding messages on gui

--- src/View.py
+++ src/View.py
@@ -6,29 +6,31 @@ import math
 
 class Tile:
     def __init__(self, mapTile, index):
-        self.mapTile =mapTile
+        self.mapTile = mapTile
         self.index_ = index
+
 
 class View(Tk):
     def __init__(self, tilesMap):
         Tk.__init__(self)
-        self.oilHex_=[]
+        self.oilHex_ = []
         self.hexagons = []
-        self.biggestTileX = 1100
-        self.biggestTileY = 850
+        self.biggestTileX = 1900
+        self.biggestTileY = 1000
         self.infoId = 0
         self.canvas = Canvas(width=1920, height=1080, bg="#AFEEEE")
         self.canvas.pack()
-        self.a_ = 4
+        self.canvas.bind("<Motion>", self.getTileByXY)
+        self.a_ = 7
         self.tilesMap_ = tilesMap
         self.Sleep = 100
-        self.initialTile = self.tilesMap_.getTile(125,75)
-        self.Pause=False
+        self.initialTile = self.tilesMap_.getTile(65, 40)
+        self.Pause = False
+        self.theBiggestDistance = 0
+        self.iterationNumber = 1
         self.drawMap()
-        self.setInitialTile(self.initialTile)
-        self.theBiggestDistance=0
 
-    def setInitialTile(self,mapTile):
+    def setInitialTile(self, mapTile):
         mapTile.setOilDensity(835)
         mapTile.setOilThickness(30)
         self.oilHex_.append(self.getHexagon(mapTile))
@@ -45,8 +47,8 @@ class View(Tk):
         for i in range(xSize):
             for j in range(ySize):
                 self.drawTile(map.getTile(i, j))
+        self.setInitialTile(self.initialTile)
         self.doChanges()
-
 
     def fromRGB(self, R, G, B):
         return "#%02x%02x%02x" % (R, G, B)
@@ -59,7 +61,7 @@ class View(Tk):
             "#0000FF"
 
         ]
-        oilColors=[
+        oilColors = [
             "#000000"
             "#1b1b20"
             "#2b2b3a"
@@ -90,20 +92,20 @@ class View(Tk):
             color = oilColors[0]
 
         index = self.canvas.create_polygon(coords[0][0],
-                                               coords[0][1],
-                                               coords[1][0],
-                                               coords[1][1],
-                                               coords[2][0],
-                                               coords[2][1],
-                                               coords[3][0],
-                                               coords[3][1],
-                                               coords[4][0],
-                                               coords[4][1],
-                                               coords[5][0],
-                                               coords[5][1],
-                                               fill=color,
-                                               outline="gray"
-                                               )
+                                           coords[0][1],
+                                           coords[1][0],
+                                           coords[1][1],
+                                           coords[2][0],
+                                           coords[2][1],
+                                           coords[3][0],
+                                           coords[3][1],
+                                           coords[4][0],
+                                           coords[4][1],
+                                           coords[5][0],
+                                           coords[5][1],
+                                           fill=color,
+                                           outline="gray"
+                                           )
         for i in coords:
             if i[0] > self.biggestTileX:
                 self.biggestTileX = i[0]
@@ -113,49 +115,64 @@ class View(Tk):
         hex = Tile(mapTile, index)
         self.hexagons.append(hex)
 
-
-    def getHexagon(self,mapTile):
+    def getHexagon(self, mapTile):
         for hex in self.hexagons:
-            if (hex.mapTile==mapTile):
+            if (hex.mapTile == mapTile):
                 return hex
 
-    def changeTileColor(self,mapTile):
+    def getHexagonByIndex(self, nearest):
+        for i in self.hexagons:
+            if i.index_ == nearest:
+                return i
+        return None
+    def changeTileColor(self, mapTile):
         self.canvas.itemconfig(self.getHexagon(mapTile).index_, fill='red')
 
-
-
-    def isInOilHex(self,tile):
+    def isInOilHex(self, tile):
         for t in self.oilHex_:
-            if (t==tile):
+            if (t == tile):
                 return True
         return False
 
     def doChanges(self):
         oilChanges = []
+        self.iterationNumber+=1
         for c in self.oilHex_:
-            changedTiles = c.mapTile.doMove(self.theBiggestDistance,self.initialTile)
+            changedTiles = c.mapTile.doMove(self.iterationNumber/2, self.initialTile)
             if changedTiles is not None:
                 for t in changedTiles:
-                    if t[0].oilThickness_>0:
-                         oilChanges.append(t[0])
-                         self.changeTileColor(t[0])
-                         distance = t[0].distance(self.initialTile)
-                         if(distance>self.theBiggestDistance):
-                             self.theBiggestDistance=distance
-                         print(self.theBiggestDistance)
-                         print(distance)
+                    if t[0].oilThickness_ > 0:
+                        oilChanges.append(t[0])
+                        self.changeTileColor(t[0])
+                        distance = t[0].distance(self.initialTile)
+#                      if (distance > self.theBiggestDistance):
+#                           self.theBiggestDistance = distance
         self.canvas.update()
         if oilChanges is not None:
-            self.oilHex_=[]
+            self.oilHex_ = []
             for t in oilChanges:
                 if not self.isInOilHex(self.getHexagon(t)):
                     self.oilHex_.append(self.getHexagon(t))
+
         if self.Pause is False:
             self.canvas.after(self.Sleep, self.doChanges)
 
+    def showTileDetails(self, tile):
+        self.canvas.delete(self.infoId)
+        onscreen = "Distance from \ninitial tile (65,40):\n(" + str(tile.mapTile.getX()) + "," + str(tile.mapTile.getY()) + ")"+ str(tile.mapTile.distance(self.initialTile))
+        print(onscreen)
+        self.infoId = self.canvas.create_text((1700, 20), anchor="nw", font=("helvetica", 12), text=onscreen)
 
-map=AllIsWaterMap.AllIsWaterMap()
-#map =Map.Map()
+    def getTileByXY(self, event):
+         print(event.x,event.y)
+ #       if (event.x<self.biggestTileX and event.y<self.biggestTileY):
+         nearest = int(self.canvas.find_closest(event.x,event.y)[0])
+         tile = self.getHexagonByIndex(nearest)
+         if tile:
+             self.showTileDetails(tile)
+
+map = AllIsWaterMap.AllIsWaterMap()
+# map =Map.Map()
 slogic = SpreadingLogic.SpreadingLogic(map)
 view = View(map)
 view.mainloop()
