c2925b09bda070591a9947fa14c34427997f3916
hkolanska <helenakolanska@gmail.com>
2019-05-27 15:50:53 +0200

    Circle neighbourhood

--- src/AllIsWaterMap.py
+++ src/AllIsWaterMap.py
@@ -1,13 +1,12 @@
 import math
-import numpy
 from src import MapTile
 
 
 class AllIsWaterMap:
     def __init__(self):
         self.listOfMapTiles = []
-        self.xSize = 470
-        self.ySize = 250
+        self.xSize = 235
+        self.ySize = 125
         a = 1
         self.xSizeMap = int((self.xSize - a * math.sqrt(3) / 2) / (a * math.sqrt(3)))
         self.ySizeMap = int(self.ySize / (3 * a)*2)
@@ -53,8 +52,8 @@ class AllIsWaterMap:
 
 
     def createNeighbourhood(self):
-        for i in range(self.xSizeMap-1):
-            for j in range(self.ySizeMap-1):
+        for i in range(2,self.xSizeMap-2):
+            for j in range(2,self.ySizeMap-2):
                 self.setNeighbours(self.listOfMapTiles[i][j])
 
     def setNeighbours(self, tile):
@@ -69,6 +68,15 @@ class AllIsWaterMap:
             if x < self.xSize - 1: neighbours.append(self.getTile(x + 1, y))
             if y > 0: neighbours.append(self.getTile(x, y - 1))
             if y < self.ySize - 1: neighbours.append(self.getTile(x, y + 1))
+            if x > 1 and y > 0: neighbours.append(self.getTile(x-2,y-1))
+            if x > 1 and y <self.ySize-1: neighbours.append(self.getTile(x-2,y+1))
+            if y > 1: neighbours.append(self.getTile(x,y-2))
+            if y < self.ySize-2: neighbours.append(self.getTile(x,y+2))
+            if x < self.xSize -1  and y >0: neighbours.append(self.getTile(x+1,y-1))
+            if x < self.xSize -1 and y <self.ySize-1:
+                print(x,y)
+                neighbours.append(self.getTile(x+1,y+1))
+
         else:
             if x > 0: neighbours.append(self.getTile(x - 1, y))
             if y > 0: neighbours.append(self.getTile(x, y - 1))
@@ -77,6 +85,12 @@ class AllIsWaterMap:
                 neighbours.append(self.getTile(x + 1, y))
                 if y > 0: neighbours.append(self.getTile(x + 1, y - 1))
                 if y < self.ySize - 1: neighbours.append(self.getTile(x + 1, y + 1))
+            if x > 0 and y > 0: neighbours.append(self.getTile(x-1,y-1))
+            if x > 0 and y <self.ySize-1: neighbours.append(self.getTile(x-1,y+1))
+            if y > 1: neighbours.append(self.getTile(x,y-2))
+            if y < self.ySize - 4 : neighbours.append(self.getTile(x,y+2))
+            if x < self.xSize - 32  and y >1: neighbours.append(self.getTile(x+2,y-1))
+            if x < self.xSize -2 and y <self.ySize-1: neighbours.append(self.getTile(x+2,y+1))
         tile.setNeighbours(neighbours)
 
 
